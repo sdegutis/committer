10addc4aa9a99a6e630b937e68d46e50d98872ad
Steven Degutis <sbdegutis@gmail.com>
2019-09-14 12:45:53 -0500

    Fix missing print.

--- tty.js
+++ tty.js
@@ -38,7 +38,9 @@ export const styles = {
   bg: { black: 40, red: 41, green: 42, yellow: 43, blue: 44, magenta: 45, cyan: 46, white: 47, },
 };
 
-export const setStyles = (...styles) => `${Esc}[${styles.join(';')}m`;
+export const setStyles = (...styles) => {
+  std.out.puts(`${Esc}[${styles.join(';')}m`);
+};
 
 export function setup() {
   // Quit if not usable
