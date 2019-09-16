56950019fcd3b0bb56114a67ec41b1e4c9a8c0a9
Steven Degutis <sbdegutis@gmail.com>
2019-09-15 23:58:35 -0500

    Udpate todo list.

--- main.js
+++ main.js
@@ -46,21 +46,9 @@
  * - [x] develop 'tty' module
  * - [x] develop 'input' module
  * - [ ] make view manager
- * - [ ] make something like magit's transient
- *   - [ ] map flags to arguments
- *   - [ ] map characters to commands
- *   - [ ] handle invalid characters
- *   - [x] make input.listen() take a stack-ish
- * - [ ] wait for os.exec() in QuickJS
- * - [ ] wrap os.exec() for convenience
- * - [ ] figure out the extra args magit means by `"..."`
- * - [ ] figure out how magit reads `git status -z --porcelain`
- * - [ ] figure out how magit gets diffs
- * - [ ] figure out how magit maps diffs to regions
- * - [ ] figure out how magit stages changes
- * - [ ] figure out how magit unstages changes
- * - [ ] figure out how magit discards regions
- * - [ ] figure out how magit discards files
+ * - [ ] make basic text editor
+ * - [ ] imitate magit's "transient" module
+ * - [ ] figure out how magic uses git
  * 
  */
 
