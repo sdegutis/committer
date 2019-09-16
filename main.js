55c5224d71ef7f3e3b6f856a93a9172b75679f6a
Steven Degutis <sbdegutis@gmail.com>
2019-09-15 23:38:55 -0500

    Moving todo list into main file.

--- main.js
+++ main.js
@@ -41,6 +41,28 @@
  *   - This means easy theming!
  */
 
+/**
+ * 
+ * - [x] develop 'tty' module
+ * - [x] develop 'input' module
+ * - [ ] make view manager
+ * - [ ] make something like magit's transient
+ *   - [ ] map flags to arguments
+ *   - [ ] map characters to commands
+ *   - [ ] handle invalid characters
+ *   - [x] make input.listen() take a stack-ish
+ * - [ ] wait for os.exec() in QuickJS
+ * - [ ] wrap os.exec() for convenience
+ * - [ ] figure out the extra args magit means by `"..."`
+ * - [ ] figure out how magit reads `git status -z --porcelain`
+ * - [ ] figure out how magit gets diffs
+ * - [ ] figure out how magit maps diffs to regions
+ * - [ ] figure out how magit stages changes
+ * - [ ] figure out how magit unstages changes
+ * - [ ] figure out how magit discards regions
+ * - [ ] figure out how magit discards files
+ * 
+ */
 
 import * as std from 'std';
 import * as os from 'os';
